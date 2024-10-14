622ee13735269f474d673c14e526707ac26b0f9a
Ch333f <adewale.o.joshua@gmail.com>
2024-10-14 13:31:16 +0100

    Configured vue env

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>
+    Welcome to Vue.js
+  </h1>
+</template>
+
+
+<script>
+  export default {
+    name: "App",
+    components: {},
+  };
+</script>
+
+
+<style>
+  #root {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
